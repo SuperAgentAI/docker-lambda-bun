5f1bda22fe72c298c488ad97de8ee5779630d238
Nick Williams <git@nickawilliams.com>
2025-01-05 18:39:10 -0800

    iac: migrated to new backend location

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-state-superagent"
-    key            = "525999333867/us-west-1/superagent/global/lambda/bun/terraform.tfstate"
-    region         = "us-west-1"
     dynamodb_table = "terraform-locks-superagent"
+    key            = "525999333867/common/docker-lambda-bun/terraform.tfstate"
+    region         = "us-west-1"
   }
 }
