001260b0ad093d7a6ac395c1743fc89c4aa49165
Nick Williams <git@nickawilliams.com>
2025-02-25 16:59:51 -0800

    chore: applied formatting to existing source files

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 export const handler = async () => {
   return new Response(
-    JSON.stringify({ status: 501, message: "empty function" }),
+    JSON.stringify({ status: 501, message: 'empty function' }),
     {
       status: 501,
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
-    }
-  );
-};
+    },
+  )
+}
